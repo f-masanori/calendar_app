d2b076d11a0010d39b554e03bc4eb65612faf0ff
masanori f <pmasanori0306@gmail.com>
2020-05-05 19:33:25 +0900

    今日の日付取得(string)

--- python3/slackbot.py
+++ python3/slackbot.py
@@ -3,18 +3,29 @@ import config
 import requests
 import json
 import mysql_conn
-try:
-    MySQLConfig = {'host': config.host,
-              'user': config.user,
-              'password': config.password,
-              'database': 'app',
-              }
+import datetime
+MySQLConfig = {
+    'host': config.host,
+    'user': config.user,
+    'password': config.password,
+    'database': 'app',
+}
+email = "test05051@test.com"
+def getTodayStr():
+    _today = datetime.date.today()
+    print(_today)
+    today = _today.strftime('%Y-%m-%d')
+    return today
+def main(): 
     mysql = mysql_conn.MySQL(MySQLConfig)
     connection = mysql.connect()
-    print(connection.is_connected())
+    # print(connection.is_connected())   
+    date = getTodayStr()
+    # print(type(date))
     cursor = connection.cursor()
     cursor.execute(
-        'SELECT event,date FROM events AS E INNER JOIN users AS U ON E.uid=U.uid WHERE email="test05051@test.com"')
+        'SELECT event,date FROM events AS E INNER JOIN users AS U ON E.uid=U.uid WHERE email=%s AND date=%s',
+        (email, date))
     rows = cursor.fetchall()
 
     event=""
@@ -32,5 +43,6 @@ try:
     #     'icon_emoji': ':smile_cat:',  # アイコン
     #     'link_names': 1,  # 名前をリンク化
     #     }))
-except mysql.connector.Error as err:
-    print("Something went wrong: {}".format(err))
+main()
+
+
